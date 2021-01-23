a9d58aa6e9e7c91dcb1aeaa32b61e89e639a16c3
Radhakrishnan C <radhakrishnan.c@jothisoftware.com>
2021-01-23 17:58:47 +0530

    Updated source

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!!!'))
 app.get('/v2', (req, res) => res.send('This is v2'))
+app.get('/v1', (req, res) => res.send('This is v1'))
 app.listen(80, () => console.log('Server ready'))
 
